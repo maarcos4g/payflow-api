3201654b3ba4395dc3492a2fa011b80d7f13ca94
Marcos Paulo <marcospaullo552@gmail.com>
2024-06-02 17:52:52 -0300

    feat: create user route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,5 +1,8 @@
 import fastify from "fastify"
 import cors from "@fastify/cors"
+import jwt from "@fastify/jwt"
+
+import { env } from '@/env'
 
 import { errorHandler } from "@/error-handler"
 
@@ -9,6 +12,10 @@ const app = fastify()
 
 app.register(cors, {
   origin: true, //url da aplicação front
+})
+
+app.register(jwt, {
+  secret: env.JWT_SECRET
 })
 
 //routes
